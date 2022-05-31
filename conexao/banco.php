52eaccf3cd6416d90b9dd098c666edb8d459770a
lucaoyz <lucasfm.1406@gmail.com>
2022-05-30 21:27:56 -0300

    conexao com banco

--- conexao/banco.php
+++ conexao/banco.php
@@ -1,11 +1,11 @@
 <?php
 
-$con = mysqli_connect("dumont", "200008", "200008", "d200008");
+$con = mysqli_connect('localhost', 'root', '', 'db200008');
 
 if (mysqli_connect_errno()) {
-    echo "Falha ao se conectar ao MySQL: " . mysqli_connect_erro();
+    echo "Falha ao se conectar ao MySQL: " . mysqli_connect_error();
 } else {
-    mysqli_select_db($con, "d200008");
+    mysqli_select_db($con, "db200008");
 }
 
 ?>
